3ea61cdd19fc9a157fcd08974b49a8896d94bbf9
Edmond Wu <edmond.wu@velocitylight.net>
2021-03-23 15:02:46 +0800

    update script

--- install_nvm.sh
+++ install_nvm.sh
@@ -1,6 +1,3 @@
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash;
 
-cat "export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm" >> ~/.zshrc
-
-source ~/.zshrc
+source ~/.zshrc;
