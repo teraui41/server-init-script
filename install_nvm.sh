2b0ddb4edef0e9f6bb0c10065aae0c14ad9c1811
Edmond Wu <edmond.wu@velocitylight.net>
2021-03-23 15:09:48 +0800

    Add console

--- install_nvm.sh
+++ install_nvm.sh
@@ -1,3 +1,11 @@
+WHITE_BG="\033[47;30m"
+END_COLOR="\033[0m"
+YELLO="\33[33m"
+
+echo "${WHITE_BG} > Start install NVM ... ${END_COLOR}";
+
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash;
 
 source ~/.zshrc;
+
+echo "${WHITE_BG} > Install completed ... ${END_COLOR}";
