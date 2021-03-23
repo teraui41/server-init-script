6fae644911d4d257693d1c7cdf58c062760a2917
Edmond Wu <edmond.wu@velocitylight.net>
2021-03-23 15:22:26 +0800

    Update certbox scripts

--- install_certbox_nginx.sh
+++ install_certbox_nginx.sh
@@ -1,16 +1,22 @@
+# https://certbot.eff.org/lets-encrypt/ubuntufocal-nginx
+
 WHITE_BG="\033[47;30m"
 END_COLOR="\033[0m"
 YELLO="\33[33m"
 
 echo "${WHITE_BG} > Start install Certbox ... ${END_COLOR}";
 
-echo "${YELLO} > Create Certbot repository ... ${END_COLOR}";
+echo "${YELLO} > Ensure that your version of snapd is up to date ... ${END_COLOR}";
 
-sudo add-apt-repository ppa:certbot/certbot;
+sudo snap install core; sudo snap refresh core
 
-echo "${YELLO} > Install certbot ... ${END_COLOR}";
+echo "${YELLO} > Install Certbot ... ${END_COLOR}";
 
-sudo apt-get update
-sudo apt-get install certbot
-sudo apt-get install python-certbot-nginx
+sudo snap install --classic certbot;
+
+echo "${YELLO} > Prepare the Certbot command ... ${END_COLOR}";
+
+sudo ln -s /snap/bin/certbot /usr/bin/certbot;
+
+sudo certbot --nginx;
 
